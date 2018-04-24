f029fa0422b9ad388f158297c8be946e85f6ce40
DanGathers <dangathersmedia@gmail.com>
2018-04-23 21:37:11 -0700

    add mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express       = require('express'),
-     app            = express(),
-     pug            = require('pug')
+      app           = express(),
+      pug           = require('pug'),
+      bodyParser    = require('body-parser'),
+      mongoose      = require('mongoose')
 
-     app.get("/", (req, res) => {
-         res.send("This is the home page!")
-     });
 
-     app.listen(4000, () => {
-         console.log("Dan Gathers Blog Server Has Started at PORT:4000!")
-     });
+mongoose.connect('mongodb://localhost/dansblog');
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + "/public"));
+
+
+
+app.get("/", (req, res) => {
+    res.send("This is the home page!")
+});
+
+app.listen(4000, () => {
+    console.log("Dan Gathers Blog Server Has Started at PORT:4000!")
+});

b59811dd85b5accd9e165e9091845f4394003145
DanGathers <dangathersmedia@gmail.com>
2018-05-09 07:51:39 -0700

    add schema routes

--- models/dansblog.js
+++ models/dansblog.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 let blogSchema = new mongoose.Schema({
     title: String,
+    image: String,
     description: String,
     blogpost: String,
     created: {type: Date, default: Date.now}

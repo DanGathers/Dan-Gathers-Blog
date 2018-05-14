1114ebed386e39e07d1c41f268221b94da51c6ed
DanGathers <dangathersmedia@gmail.com>
2018-05-14 06:33:25 -0700

    add user & comment routes w/ styles

--- models/dansblog.js
+++ models/dansblog.js
@@ -4,6 +4,14 @@ const mongoose = require('mongoose');
 let blogSchema = new mongoose.Schema({
     title: String,
     image: String,
+    id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
     description: String,
     blogpost: String,
     created: {type: Date, default: Date.now},

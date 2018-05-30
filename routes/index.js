5207e3b107c8d16ea6b986a475ca4046407e0681
DanGathers <dangathersmedia@gmail.com>
2018-05-30 05:24:59 -0700

    add login routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,33 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
+
 
 
 // root route 
 router.get("/", function(req, res) {
     res.render("landing");
 });
+
+// REGISTER ROUTE
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+
+// LOGIN ROUTES
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// LOGOUT ROUTE
+router.get("/logout", (req, res) => {
+    // handle with Passport
+    res.send("Logging Out");
+});
+
+// OAuth with Google+
+router.get("/google", (req, res) => {
+    // handle with Passport
+    res.send("Logging in with Google+")
+})
 
 module.exports = router;
